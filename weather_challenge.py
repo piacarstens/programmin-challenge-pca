84a4d3f471d4cf8a6639791e564ad2068c653d4c
piacarstens <42817573+piacarstens@users.noreply.github.com>
2018-09-01 18:56:51 +0200

    Get data
    In this step I extract data from the csv-file and check if i got the right ones.

--- weather_challenge.py
+++ weather_challenge.py
@@ -2,7 +2,21 @@
 # -------------------------------------------
 
 # read file
-
 f=open('weather.csv')
-file = f.read()
-print(file)
+#file = f.read()
+#print(file)
+
+# get data
+days  = []
+min_T = []
+max_T = []
+
+for line in f:
+    fields =line.split(",")
+    days.append(fields[0])
+    min_T.append(fields[1])
+    max_T.append(fields[2])
+    
+print(days)
+print(min_T)
+print(max_T)
