6ff396f91660dd767ba60ba9f173bb087d099ee8
piacarstens <42817573+piacarstens@users.noreply.github.com>
2018-09-01 21:16:20 +0200

    difference between temperatures
    Added difference between Tmin and Tmax. Added import numpy. Print out dT to check if it works fine.

--- weather_challenge.py
+++ weather_challenge.py
@@ -1,8 +1,12 @@
 # weather challenge - pia carstens - 1.9.2018
 # -------------------------------------------
+import numpy as np
+
 
 # read file
 f=open('weather.csv')
+
+
 #file = f.read()
 #print(file)
 
@@ -19,7 +23,11 @@ for line in f:
     min_T.append(int(fields[2]))
     max_T.append(int(fields[1]))
     
-print(days)
-print(min_T)
-print(max_T)
+#print(days)
+#print(min_T)
+#print(max_T)
     
+# calculate difference between Tmax and Tmin
+dT=np.subtract(max_T,min_T)
+
+print(dT)
