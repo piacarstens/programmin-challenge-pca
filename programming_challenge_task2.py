91bdc913cff509a1186dbd74a19efc16c9343859
piacarstens <42817573+piacarstens@users.noreply.github.com>
2018-09-02 16:55:00 +0200

    Defined methods read_data and get_result
    I defined the two methods read_data and get_result. I decided to split the functions up (compared to my matlab code) in order to be able to replace components and assure reusability. I might like to use a different method to calculate a result for my data later on.

--- programming_challenge_task2.py
+++ programming_challenge_task2.py
@@ -5,27 +5,43 @@ Created on Sat Sep  1 21:36:49 2018
 @author: pia carstens
 """
 # programming challenge - task 2 - combined weather and football
+
 import numpy as np
+
+def read_data(f,idx_target,idx_val1,idx_val2):
+    """ function for reading data for 3 defined indices"""
+    target_value  = []
+    val1 = []
+    val2 = []
+    for line in f:
+        fields =line.split(",")
+        target_value.append(fields[idx_target])
+        val1.append(int(fields[idx_val1]))
+        val2.append(int(fields[idx_val2]))
+    return(target_value,val1,val2)
+    
+
+def get_result(target_value,val1,val2):
+    """ function for calculating the target_value of the minimum 
+    difference of val1 and val2"""
+    val_diff = list(abs(np.subtract(val1,val2)))
+    idx_min_val_diff = val_diff.index(min(val_diff))
+    return target_value[idx_min_val_diff]
+
+
 
 # read file 
 f = open('football.csv')
-
-
-# get data
-target_value  = []
-val1 = []
-val2 = []
-
 header = f.readline()
 
-for line in f:
-    fields =line.split(",")
-    target_value.append(fields[0])
-    val1.append(int(fields[5]))
-    val2.append(int(fields[6]))
-    
-# get index of smallest temperature difference
-val_diff = list(abs(np.subtract(val1,val2)))
-idx_min_val_diff = val_diff.index(min(val_diff))
+# assign indices
+idx_target = 0
+idx_val1 = 5
+idx_val2 = 6
 
-print('Day of minimum temperature spread: ', target_value[idx_min_val_diff] )
+# call functions to read data from file and do calculation/get result
+(target_value,val1,val2) = read_data(f,idx_target,idx_val1,idx_val2) 
+result = get_result(target_value,val1,val2)
+
+# print result
+print('Day of minimum temperature spread: ', result )
