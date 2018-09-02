87ca48125f9fbff252039cf05a0848b3bb6ad7c2
piacarstens <42817573+piacarstens@users.noreply.github.com>
2018-09-02 23:06:56 +0200

    Restructured for general attempt
    In this last step I did some restructuring to allow for a more general attempt. I defined the programming_challenge-class that is used to solve the task now.

--- programming_challenge_task2.py
+++ programming_challenge_task2.py
@@ -8,46 +8,54 @@ Created on Sat Sep  1 21:36:49 2018
 
 import numpy as np
 
-def read_header(f,h_target,h_val1,h_val2):
-    """in this function indices of header values are extracted"""
-    header = f.readline()
-    header_fields = header.split(",")
-    idx_target = header_fields.index(h_target)
-    idx_val1 = header_fields.index(h_val1)
-    idx_val2 = header_fields.index(h_val2)
-    return(idx_target,idx_val1,idx_val2)
+class programming_challenge:
+    result = 'unknown'
+    
+    def __init__(self,filename,h_target,h_val1,h_val2):
+        """ filename: input file in csv format
+            h_val1 and h_val2: we are looking for the difference of those two 
+                columns
+            h_target: header name of target value """
+        self.f = open(filename)
+        self.h_target = h_target
+        self.h_val1 = h_val1
+        self.h_val2 = h_val2
+    
+    def get_data(self):
+        """ get header indices and extract 3 data columns """
+        header = self.f.readline()
+        header_fields = header.split(",")
+        idx_target = header_fields.index(self.h_target)
+        idx_val1 = header_fields.index(self.h_val1)
+        idx_val2 = header_fields.index(self.h_val2)
 
-def read_data(f,idx_target,idx_val1,idx_val2):
-    """ function for reading data for 3 defined indices"""
-    target_value  = []
-    val1 = []
-    val2 = []
-    for line in f:
-        fields =line.split(",")
-        target_value.append(fields[idx_target])
-        val1.append(int(fields[idx_val1]))
-        val2.append(int(fields[idx_val2]))
-    return(target_value,val1,val2)
+        self.target_value  = []
+        self.val1 = []
+        self.val2 = []
+        for line in self.f:
+            fields =line.split(",")
+            self.target_value.append(fields[idx_target])
+            self.val1.append(int(fields[idx_val1]))
+            self.val2.append(int(fields[idx_val2]))
+    
+    def get_result(self):
+        """ get the target_value of the minimum difference between val1 and val2 """
+        val_diff = list(abs(np.subtract(self.val1,self.val2)))
+        idx_min_val_diff = val_diff.index(min(val_diff))
+        
+        self.result = self.target_value[idx_min_val_diff]
 
-def get_result(target_value,val1,val2):
-    """ function for calculating the target_value of the minimum 
-    difference of val1 and val2"""
-    val_diff = list(abs(np.subtract(val1,val2)))
-    idx_min_val_diff = val_diff.index(min(val_diff))
-    return target_value[idx_min_val_diff]
-
-# read file 
-f = open('football.csv')
-
-# assign headers
-h_target='Team'
-h_val1='Goals'
-h_val2='Goals Allowed'
-
-# call functions to read data from file and do calculation/get result
-(idx_target,idx_val1,idx_val2) = read_header(f,h_target,h_val1,h_val2)
-(target_value,val1,val2) = read_data(f,idx_target,idx_val1,idx_val2) 
-result = get_result(target_value,val1,val2)
-
-# print result
-print('Day of minimum temperature spread: ', result )
+    
+if __name__ == "__main__":
+    
+    # weather challenge
+    weather_challenge  = programming_challenge('weather.csv','Day','MxT','MnT')
+    weather_challenge.get_data()
+    weather_challenge.get_result()
+    print('Day of minimum temperature spread: ', weather_challenge.result)
+    
+    # football challenge
+    football_challenge = programming_challenge('football.csv','Team','Goals','Goals Allowed')
+    football_challenge.get_data()
+    football_challenge.get_result()
+    print('Team with smalles goal spread: ', football_challenge.result)
