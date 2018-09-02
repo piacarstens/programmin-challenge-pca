e6e9c5e3c4a2684a3df88effde56f2b0d8128d3a
piacarstens <42817573+piacarstens@users.noreply.github.com>
2018-09-02 17:10:38 +0200

    Get header indices
    Since I am only supposed to know the indices of the columns in question for task 1, I assume that a more general approach is to look for the indices of given header values. Reading the header and returning the indices is implemented in the new function read_header.

--- programming_challenge_task2.py
+++ programming_challenge_task2.py
@@ -8,6 +8,15 @@ Created on Sat Sep  1 21:36:49 2018
 
 import numpy as np
 
+def read_header(f,h_target,h_val1,h_val2):
+    """in this function indices of header values are extracted"""
+    header = f.readline()
+    header_fields = header.split(",")
+    idx_target = header_fields.index(h_target)
+    idx_val1 = header_fields.index(h_val1)
+    idx_val2 = header_fields.index(h_val2)
+    return(idx_target,idx_val1,idx_val2)
+
 def read_data(f,idx_target,idx_val1,idx_val2):
     """ function for reading data for 3 defined indices"""
     target_value  = []
@@ -19,7 +28,6 @@ def read_data(f,idx_target,idx_val1,idx_val2):
         val1.append(int(fields[idx_val1]))
         val2.append(int(fields[idx_val2]))
     return(target_value,val1,val2)
-    
 
 def get_result(target_value,val1,val2):
     """ function for calculating the target_value of the minimum 
@@ -28,18 +36,16 @@ def get_result(target_value,val1,val2):
     idx_min_val_diff = val_diff.index(min(val_diff))
     return target_value[idx_min_val_diff]
 
-
-
 # read file 
 f = open('football.csv')
-header = f.readline()
 
-# assign indices
-idx_target = 0
-idx_val1 = 5
-idx_val2 = 6
+# assign headers
+h_target='Team'
+h_val1='Goals'
+h_val2='Goals Allowed'
 
 # call functions to read data from file and do calculation/get result
+(idx_target,idx_val1,idx_val2) = read_header(f,h_target,h_val1,h_val2)
 (target_value,val1,val2) = read_data(f,idx_target,idx_val1,idx_val2) 
 result = get_result(target_value,val1,val2)
 
